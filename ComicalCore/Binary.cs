e6d70b8acde2e87ae8e46161062dae4d057d2f5b
calbonaler <calbonaler.prime@gmail.com>
2024-10-12 15:19:01 +0900

    refactor: Remove unused usings

--- ComicalCore/Binary.cs
+++ ComicalCore/Binary.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Buffers;
-using System.Buffers.Binary;
 using System.IO;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace Comical.Core
--- ComicalCore/Comic.cs
+++ ComicalCore/Comic.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
